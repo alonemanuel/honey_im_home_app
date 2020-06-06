d26419ac0acc4f9737d780408fe0a94c34c30e84
Alon Emanuel <alon@alonemanuel.com>
2020-06-06 03:07:10 +0300

    Broadcasting maybe working

--- app/src/main/java/com/example/honeyhome/LocationTracker.kt
+++ app/src/main/java/com/example/honeyhome/LocationTracker.kt
@@ -1,39 +1,104 @@
 package com.example.honeyhome
 
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Bundle
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices.getFusedLocationProviderClient
+import timber.log.Timber
 
-class LocationTracker(context: Context) {
-    val longtitude: Int = 0
-    val altitude: Int = 0
-    val accuracy: Int = 0
-    val contex: Context = context
+class LocationTracker(activity: MainActivity) : GoogleApiClient.ConnectionCallbacks,
+    GoogleApiClient.OnConnectionFailedListener {
+    var longtitude: Double = 0.0
+    var latitude: Double = 0.0
+    var accuracy: Float = 0f
+    val activity: MainActivity = activity
+    var isTracking: Boolean = false
+    private val MY_PERMISSIONS_REQUEST_LOCATION = 99;
+    lateinit var fusedLocationClient: FusedLocationProviderClient
 
     init {
 
     }
 
     fun startTracking() {
+        isTracking = true
+        getPermissionAndTrack()
+        sendBroadcast(activity)
+    }
 
-        if (ActivityCompat.checkSelfPermission(
-                contex,
-                android.Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
+    fun getPermissionAndTrack() {
+        val hasLocationPermission: Boolean = ActivityCompat.checkSelfPermission(
+            activity, android.Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
 
-        ) {
-            // track
-            Toast.makeText(contex, "Starting to track", Toast.LENGTH_LONG).show()
+        if (hasLocationPermission) {
+            Toast.makeText(activity.applicationContext, "permission granted", Toast.LENGTH_LONG)
+                .show()
+            fusedLocationClient = getFusedLocationProviderClient(activity)
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener { location: Location? ->
+                    // Got last known location. In some rare situations this can be null.
+                    if (location == null) {
+                        Toast.makeText(
+                            activity,
+                            "Location is null for some reason",
+                            Toast.LENGTH_LONG
+                        )
+                            .show()
+
+                    } else {
+                        longtitude = location.longitude
+                        latitude = location.latitude
+                        accuracy = location.accuracy
+
+                    }
+                }
+
 
         } else {
-            // log error
-            Toast.makeText(contex, "No permission erorrrorror", Toast.LENGTH_LONG).show()
+            val permissions = arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION)
+            ActivityCompat.requestPermissions(
+                activity,
+                permissions,
+                MY_PERMISSIONS_REQUEST_LOCATION
+            )
+            Toast.makeText(activity, "permission denied", Toast.LENGTH_LONG).show()
+
+
         }
     }
 
-    fun stopTracking() {
+
+    fun sendBroadcast(context: Context) {
+        val intent = Intent()
+        intent.action = "start_tracking"
+        context.sendBroadcast(intent)
 
     }
+
+
+    fun stopTracking() {
+        isTracking = false
+    }
+
+    override fun onConnected(p0: Bundle?) {
+        Timber.i("connected")
+    }
+
+    override fun onConnectionSuspended(p0: Int) {
+        Timber.i("Connection suspended, please reconnect")
+    }
+
+    override fun onConnectionFailed(p0: ConnectionResult) {
+        Timber.i("connection failed")
+    }
+
 
 }
--- app/src/main/java/com/example/honeyhome/MainActivity.kt
+++ app/src/main/java/com/example/honeyhome/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.honeyhome
 
-import android.content.pm.PackageManager
-import android.location.Location
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.os.Looper
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
+import androidx.databinding.DataBindingUtil
+import com.example.honeyhome.databinding.ActivityMainBinding
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.api.GoogleApiClient
 import com.google.android.gms.location.*
@@ -16,19 +19,32 @@ import timber.log.Timber
 
 class MainActivity : GoogleApiClient.ConnectionCallbacks,
     GoogleApiClient.OnConnectionFailedListener, AppCompatActivity() {
-    public lateinit var locationTracker: LocationTracker
+    lateinit var locationTracker: LocationTracker
     private lateinit var locationCallback: LocationCallback
-
     lateinit var locationRequest: LocationRequest
     private lateinit var fusedLocationClient: FusedLocationProviderClient
-    private var mGoogleApiClient: GoogleApiClient? = null
+    private lateinit var mGoogleApiClient: GoogleApiClient
+    lateinit var binding: ActivityMainBinding
 
-    private final val MY_PERMISSIONS_REQUEST_LOCATION = 99;
+    val myReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            binding.longtitudeTextView.text = locationTracker.longtitude.toString()
+            binding.latitudeTextView.text = locationTracker.latitude.toString()
+            binding.accuracyTextView.text = locationTracker.accuracy.toString()
+
+            Timber.i(
+                "got intent with data: name=" + (intent?.getStringExtra("name"))
+            )
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        var activity: MainActivity = this
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val activity: MainActivity = this
         locationCallback = object : LocationCallback() {
             override fun onLocationResult(p0: LocationResult?) {
                 p0 ?: return
@@ -39,100 +55,52 @@ class MainActivity : GoogleApiClient.ConnectionCallbacks,
                 super.onLocationResult(p0)
             }
         }
-
-        // SO
+        registerReceiver(myReceiver, IntentFilter("start_tracking"))
         locationRequest = LocationRequest.create()
-
-        getLocation()
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-
         mGoogleApiClient = GoogleApiClient.Builder(this)
             .addConnectionCallbacks(this)
             .addOnConnectionFailedListener(this)
             .addApi(LocationServices.API)
             .build()
-        var isTrackingLocation: Boolean = false
+//        getLocation()
+
         var buttonTrackLocation: Button = findViewById(R.id.button_track_location)
         locationTracker = LocationTracker(this)
         buttonTrackLocation.setOnClickListener {
             buttonTrackLocationListener(
-                activity,
                 locationTracker
             )
         }
-
     }
 
-    private fun getLocation() {
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-        val LOCATION_INTERVAL = 2000L
-        locationRequest.setInterval(LOCATION_INTERVAL)
-        locationRequest.setFastestInterval(LOCATION_INTERVAL)
-        val fusedLocationProviderApi = LocationServices.FusedLocationApi
-        val googleApiClient = GoogleApiClient.Builder(this)
-            .addApi(LocationServices.API)
-            .addConnectionCallbacks(this)
-            .addOnConnectionFailedListener(this)
-            .build()
-        if (googleApiClient != null) {
-            googleApiClient.connect()
-        }
-    }
+//    private fun getLocation() {
+//        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+//        val LOCATION_INTERVAL = 2000L
+//        locationRequest.setInterval(LOCATION_INTERVAL)
+//        locationRequest.setFastestInterval(LOCATION_INTERVAL)
+//
+//        // removed null check
+//        mGoogleApiClient.connect()
+//    }
 
 
-    fun buttonTrackLocationListener(activity: MainActivity, locationTracker: LocationTracker) {
-        val hasLocationPermission: Boolean = ActivityCompat.checkSelfPermission(
-            activity,
-            android.Manifest.permission.ACCESS_FINE_LOCATION
-        ) == PackageManager.PERMISSION_GRANTED
-
-        if (hasLocationPermission) {
-            Toast.makeText(this.applicationContext, "permission granted", Toast.LENGTH_LONG).show()
-            fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-            fusedLocationClient.lastLocation
-                .addOnSuccessListener { location: Location? ->
-                    // Got last known location. In some rare situations this can be null.
-                    Toast.makeText(this, location?.longitude.toString(), Toast.LENGTH_LONG).show()
-                }
-            locationTracker.startTracking()
-
+    fun buttonTrackLocationListener(locationTracker: LocationTracker) {
+        var trackingButton = findViewById<Button>(R.id.button_track_location)
+        if (locationTracker.isTracking) {
+            locationTracker.stopTracking()
+            trackingButton.text = "Start Tracking Location"
         } else {
-            val permissions = arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION)
-            ActivityCompat.requestPermissions(
-                activity,
-                permissions,
-                MY_PERMISSIONS_REQUEST_LOCATION
-            )
-            Toast.makeText(this.applicationContext, "permission denied", Toast.LENGTH_LONG).show()
-
+            locationTracker.startTracking()
+            trackingButton.text = "Stop Tracking Location"
         }
-
 
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-        when (requestCode) {
-            MY_PERMISSIONS_REQUEST_LOCATION -> {
-                if (grantResults.count() > 0
-                    && grantResults[0] == PackageManager.PERMISSION_GRANTED
-
-                ) {
-                    locationTracker.startTracking()
-
-                } else {
-                    Toast.makeText(this.applicationContext, "permission denied", Toast.LENGTH_LONG)
-                        .show()
-
-                }
-            }
-
-        }
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(myReceiver)
     }
 
     override fun onResume() {
